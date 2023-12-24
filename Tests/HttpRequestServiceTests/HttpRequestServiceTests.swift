ede73308590b7900a30c39de0862f30d6ad7e746
Nagaraju0323 <nagarajukios85@gmail.com>
2023-12-24 22:52:58 +0530

    Text name

--- Tests/HttpRequestServiceTests/HttpRequestServiceTests.swift
+++ Tests/HttpRequestServiceTests/HttpRequestServiceTests.swift
@@ -3,10 +3,8 @@ import XCTest
 
 final class HttpRequestServiceTests: XCTestCase {
     func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+       
+        XCTAssertEqual(HttpRequestService().name, "Hello")
+        
     }
 }
